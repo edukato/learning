d6809d8dd0b3d9c548754d8ad8a8a58aa3cd3dde
Daniil Boiko <Daniel.boyko.al@gmail.com>
2017-06-22 19:34:17 +0300

    Дублирование кода

--- config.py
+++ config.py
@@ -27,36 +27,3 @@ app_config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig
 }  # config.py
-
-
-class Config(object):
-    """
-    Common configurations
-    """
-
-    # Put any configurations here that are common across all environments
-
-
-class DevelopmentConfig(Config):
-    """
-    Development configurations
-    """
-
-    DEBUG = True
-    SQLALCHEMY_ECHO = True
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class ProductionConfig(Config):
-    """
-    Production configurations
-    """
-
-    DEBUG = False
-
-
-app_config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig
-}
