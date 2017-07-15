32e6a5da8fbc63c84980a64536b68c31dbdeceda
Daniil Boiko <Daniel.boyko.al@gmail.com>
2017-07-16 01:41:18 +0300

    Страница ученика в админке

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,10 @@
+import datetime
+
 from flask import flash, redirect, render_template, url_for, abort
 from flask_login import login_required, current_user
 
 from . import admin
-from ..models import Client, Subject, TrainingChoice, Task
+from ..models import Client, Subject, TrainingChoice, Task, Schedule, Teacher
 from .. import db
 from .forms import SubjectEditForm, SubjectAddForm, ChoiceAddForm, ChoiceEditForm, TaskAddForm
 
@@ -169,3 +171,63 @@ def task_add(id):
             flash('Ошибка.')
         return redirect(url_for('admin.subject', id=id))
     return render_template('admin/task_add.html', form=form, subject=subject, title='Добавить задание')
+
+
+@admin.route('/admin/student/<int:id>', methods=['GET', 'POST"'])
+@login_required
+def student(id):
+    check_admin()
+
+    student = Client.query.get_or_404(id)
+    if student.status != 1:
+        flash('Это не ученик')
+        return redirect(url_for('admin.clients'))
+
+    weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+    schedule = Schedule.query.filter((Schedule.client_id == id) & (Schedule.time > datetime.datetime.now())).all()
+
+    months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября',
+              'декабря']
+
+    for schedule_item in schedule:
+        schedule_item.day = schedule_item.time.day
+        schedule_item.subject_name = Subject.query.get_or_404(schedule_item.subject_id).subject
+        schedule_item.dow = schedule_item.time.weekday()
+        schedule_item.day_of_week = weekdays[schedule_item.dow]
+        schedule_item.date = str(schedule_item.time.day) + ' ' + months[
+            schedule_item.time.month - 1] + ' ' + str(schedule_item.time.year)
+        schedule_teacher = Client.query.get_or_404(Teacher.query.get_or_404(schedule_item.teacher_id).login_id)
+        schedule_item.teacher_name = schedule_teacher.last_name + ' ' + schedule_teacher.first_name[0] + '. ' + \
+                                     schedule_teacher.middle[0] + '.'
+
+    now = datetime.datetime.now()
+    byweeks = [[[], [], [], [], [], [], []]]
+    endweek = now + datetime.timedelta(days=(7 - now.weekday()))
+    endweek = endweek.replace(hour=0, minute=0, second=0, microsecond=0)
+    for schedule_item in schedule:
+        if schedule_item.time < endweek:
+            byweeks[0][schedule_item.dow].append(schedule_item)
+        else:
+            try:
+                byweeks[(schedule_item.time - endweek).days // 7 + 1][schedule_item.dow].append(schedule_item)
+            except IndexError:
+                for _ in range(((schedule_item.time - endweek).days // 7 + 1) - len(byweeks) + 1):
+                    byweeks.append([[], [], [], [], [], [], []])
+                byweeks[(schedule_item.time - endweek).days // 7 + 1][schedule_item.dow].append(schedule_item)
+
+    return render_template('admin/student.html', student=student, schedule=byweeks, weekdays=weekdays,
+                           title=(student.last_name + student.first_name + student.middle))
+
+
+@admin.route('/admin/teacher/<int:id>', methods=['GET', 'POST'])
+@login_required
+def teacher(id):
+    check_admin()
+
+    teacher = Client.query.get_or_404
+    if teacher.status != 3:
+        flash('Это не учитель')
+        return redirect(url_for('admin.clients'))
+
+    return render_template('admin/teacher.html', teacher=teacher,
+                           title=(teacher.last_name + teacher.first_name + teacher.middle))
